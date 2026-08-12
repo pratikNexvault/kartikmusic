da679f94a441975408c3e3c5b23d17df004f1532
pratikNexvault <pratik.nexvault@gmail.com>
2026-08-13 00:25:27 +0530

    Brand as Sora Music, point support links to @weegoes

--- config.py
+++ config.py
@@ -30,8 +30,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/soramusicrobot")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/soramusicrobot")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/weegoes")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/weegoes")
 
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", "False").lower() == "true"
         self.AUTO_END: bool = getenv("AUTO_END", "False").lower() == "true"
