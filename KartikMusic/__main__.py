62cdee145cde2bd6e787e568f4ec6970cb79e447
pratikNexvault <pratik.nexvault@gmail.com>
2026-08-13 00:44:31 +0530

    fix: use app.run() instead of custom idle() to fix polling in pm2

--- KartikMusic/__main__.py
+++ KartikMusic/__main__.py
@@ -6,24 +6,12 @@
 
 import asyncio
 import importlib
-import signal
-from contextlib import suppress
 
 from KartikMusic import Kartik, app, config, db, logger, stop, thumb, userbot, yt
 from KartikMusic.plugins import all_modules
 
 
-async def idle():
-    loop = asyncio.get_running_loop()
-    stop_event = asyncio.Event()
-
-    for sig in (signal.SIGINT, signal.SIGTERM, signal.SIGABRT):
-        with suppress(NotImplementedError):
-            loop.add_signal_handler(sig, stop_event.set)
-    await stop_event.wait()
-
-
-async def main():
+async def startup():
     await db.connect()
     await thumb.start()
     await app.boot()
@@ -42,12 +30,10 @@ async def main():
     app.bl_users.update(await db.get_blacklisted())
     logger.info(f"Loaded {len(app.sudoers)} sudo users.")
 
-    await idle()
-    asyncio.create_task(stop())
+
+async def shutdown():
+    await stop()
 
 
 if __name__ == "__main__":
-    try:
-        asyncio.get_event_loop().run_until_complete(main())
-    except KeyboardInterrupt:
-        pass
+    app.run(startup(), shutdown())
